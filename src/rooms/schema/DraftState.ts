21b8e58bbde5cc2cb9230cadba2d710bf06cafc6
Áron Gida <aron.gida@bosch.com>
2024-04-18 16:45:48 +0200

    added more items

--- src/rooms/schema/DraftState.ts
+++ src/rooms/schema/DraftState.ts
@@ -28,7 +28,7 @@ export class Item extends Schema {
 export class DraftState extends Schema {
   @type(Player) player: Player = new Player();
   @type([Item]) shop: ArraySchema<Item> = new ArraySchema<Item>();
-  @type("number") shopSize: number = 3;
+  @type("number") shopSize: number = 5;
 }
 
 
