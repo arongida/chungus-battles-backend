297d2f3cad45b12450bc4bea5bf638fe36ce665b
Áron Gida <aron.gida@bosch.com>
2024-06-19 11:20:25 +0200

    use tabs

--- src/rooms/schema/DraftState.ts
+++ src/rooms/schema/DraftState.ts
@@ -4,11 +4,11 @@ import { Item } from './ItemSchema';
 import { Talent } from './TalentSchema';
 
 export class DraftState extends Schema {
-  @type(Player) player: Player = new Player();
-  @type([Item]) shop: ArraySchema<Item> = new ArraySchema<Item>();
-  @type([Talent]) availableTalents: ArraySchema<Talent> =
-    new ArraySchema<Talent>();
-  @type('number') shopSize: number = 6;
-  @type('number') shopRefreshCost: number = 2;
-  @type('number') remainingTalentPoints: number = 0;
+	@type(Player) player: Player = new Player();
+	@type([Item]) shop: ArraySchema<Item> = new ArraySchema<Item>();
+	@type([Talent]) availableTalents: ArraySchema<Talent> =
+		new ArraySchema<Talent>();
+	@type('number') shopSize: number = 6;
+	@type('number') shopRefreshCost: number = 2;
+	@type('number') remainingTalentPoints: number = 0;
 }
