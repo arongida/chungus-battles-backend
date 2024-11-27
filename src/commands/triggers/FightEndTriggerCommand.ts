3aef9d56cf7053f14847ab32c1db240729bd17ef
arongida <arkheonos@gmail.com>
2024-11-27 20:38:10 +0100

    new rogue tier 1 item set

--- src/commands/triggers/FightEndTriggerCommand.ts
+++ src/commands/triggers/FightEndTriggerCommand.ts
@@ -3,18 +3,31 @@ import { Talent } from '../../talents/schema/TalentSchema';
 import { TalentBehaviorContext } from '../../talents/behavior/TalentBehaviorContext';
 import { TriggerType } from '../../common/types';
 import { FightRoom } from '../../rooms/FightRoom';
+import { BehaviorContext } from '../../common/BehaviorContext';
+import { ItemCollection } from '../../item-collections/schema/ItemCollectionSchema';
 
 export class FightEndTriggerCommand extends Command<FightRoom> {
 	execute() {
-    const fightEndTalents: Talent[] = this.state.player.talents.filter((talent) =>
-      talent.tags.includes(TriggerType.FIGHT_END)
-    );
-    const fightEndTalentsContext: TalentBehaviorContext = {
-      client: this.state.playerClient,
-      attacker: this.state.player,
-    };
-    fightEndTalents.forEach((talent) => {
-      talent.executeBehavior(fightEndTalentsContext);
-    });
+		const fightEndBehaviorContext: BehaviorContext = {
+			client: this.state.playerClient,
+			attacker: this.state.player,
+			defender: this.state.enemy,
+		};
+
+		const fightEndTalents: Talent[] = this.state.player.talents.filter((talent) =>
+			talent.tags.includes(TriggerType.FIGHT_END)
+		);
+
+		fightEndTalents.forEach((talent) => {
+			talent.executeBehavior(fightEndBehaviorContext);
+		});
+
+		//handle on fight start item collections
+		const onFightEndItemCollections: ItemCollection[] = this.state.player.activeItemCollections.filter(
+			(itemCollection) => itemCollection.tags.includes(TriggerType.FIGHT_END)
+		);
+		onFightEndItemCollections.forEach((itemCollection) => {
+			itemCollection.executeBehavior(fightEndBehaviorContext);
+		});
 	}
 }
