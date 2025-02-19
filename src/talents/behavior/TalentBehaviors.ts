2ae541cb3a21a2b2777603fc75cc00109439100d
takacsvilmos <vilmos.takacs@gmail.com>
2025-02-19 14:00:38 +0100

    joker talent added

--- src/talents/behavior/TalentBehaviors.ts
+++ src/talents/behavior/TalentBehaviors.ts
@@ -4,6 +4,7 @@ import { increaseStats } from '../../common/utils';
 import { AffectedStats, Item } from '../../items/schema/ItemSchema';
 import { Talent } from '../schema/TalentSchema';
 import { OnDamageTriggerCommand } from '../../commands/triggers/OnDamageTriggerCommand';
+import { rollTheDice } from '../../common/utils';
 
 export const TalentBehaviors = {
 	[TalentType.RAGE]: (context: TalentBehaviorContext) => {
@@ -249,7 +250,7 @@ export const TalentBehaviors = {
 		if (!defender) return;
 		const attackSpeedBonus = Math.min(0.01 + (attacker.income * 0.01)) * defender.attackSpeed;
 
-    if (defender.attackSpeed <= 0.1) return;
+		if (defender.attackSpeed <= 0.1) return;
 
 		attacker.attackSpeed += attackSpeedBonus;
 		defender.attackSpeed -= attackSpeedBonus;
@@ -524,10 +525,6 @@ export const TalentBehaviors = {
 			attacker: attacker,
 		});
 
-		function rollTheDice(min: number, max: number) {
-			return Math.floor(Math.random() * (max - min + 1)) + min;
-		}
-
 		defender.takeDamage(damage, client);
 
 		client.send('combat_log', `${attacker.name} rolls the dice and deals ${damage} damage!`);
@@ -561,15 +558,76 @@ export const TalentBehaviors = {
 			talent.savedValues = { damage: newDamage };
 			const getDamageAfterDefense = defender.getDamageAfterDefense(newDamage);
 
-      commandDispatcher.dispatch(new OnDamageTriggerCommand(), {
-        defender: defender,
-        damage: getDamageAfterDefense,
-        attacker: attacker,
-      });
+			commandDispatcher.dispatch(new OnDamageTriggerCommand(), {
+				defender: defender,
+				damage: getDamageAfterDefense,
+				attacker: attacker,
+			});
 
 			defender.takeDamage(getDamageAfterDefense, client);
 			client.send('combat_log', `${attacker.name} deals ${getDamageAfterDefense} damage with the magic ring!`);
 			client.send('trigger_talent', { playerId: attacker.playerId, talentId: TalentType.MAGIC_RING_WEAPON });
 		}
 	},
+	[TalentType.JOKER]: (context: TalentBehaviorContext) => {
+		const { attacker, client } = context;
+
+		let stat = "";
+		let amount = 0;
+
+		const randomBonus = rollTheDice(1, 9);
+		if (randomBonus === 1) {
+			amount = 5 + attacker.level;
+			attacker.initialStats.hp += amount;
+			attacker.hp += amount;
+			stat = "hp";
+		} else if (randomBonus === 2) {
+			amount = attacker.level;
+			attacker.initialStats.accuracy += amount;
+			attacker.accuracy += amount;
+			stat = "accuracy";
+		} else if (randomBonus === 3) {
+			amount = 1 + attacker.level;
+			attacker.initialStats.strength += amount;
+			attacker.strength += amount;
+			stat = "strength";
+		} else if (randomBonus === 4) {
+			amount = 9 + attacker.level;
+			attacker.initialStats.defense += amount;
+			attacker.defense += amount;
+			stat = "defense";
+		} else if (randomBonus === 5) {
+			amount = attacker.level * 0.1;
+			attacker.initialStats.flatDmgReduction += amount;
+			attacker.flatDmgReduction += amount;
+			stat = "flat damage reduction";
+		} else if (randomBonus === 6) {
+			amount = 10 + attacker.level;
+			attacker.initialStats.dodgeRate += amount;
+			attacker.dodgeRate += amount;
+			stat = "dodge rate";
+		} else if (randomBonus === 7) {
+			amount = attacker.level * 0.05;
+			attacker.initialStats.attackSpeed += amount;
+			attacker.attackSpeed += amount;
+			stat = "attack speed";
+		} else if (randomBonus === 8) {
+			amount = 0 + attacker.level;
+			attacker.initialStats.income += amount;
+			attacker.income += amount;
+			stat = "income";
+		} else if (randomBonus === 9) {
+			amount = attacker.level * 0.1;
+			attacker.initialStats.hpRegen += amount;
+			attacker.hpRegen += amount;
+			stat = "hp regeneration";
+		}
+
+		client.send('combat_log', `${attacker.name} gets ${amount} bonus ${stat} from Joker talent.`);
+		client.send('trigger_talent', {
+			playerId: attacker.playerId,
+			talentId: TalentType.JOKER,
+		});
+	},
 };
+
