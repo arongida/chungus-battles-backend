7d1c52af60fe8b73cf66cfbabed5b1eb1f82805e
takacsvilmos <vilmos.takacs@gmail.com>
2025-03-12 15:53:26 +0100

    mongoose connection issue solved

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 export default {
     preset: 'ts-jest', // Use ts-jest for TypeScript support
     testEnvironment: 'node', // Needed for Colyseus server tests
-    testTimeout: 10000,
+    testTimeout: 60000,
     transform: {
       '^.+\\.ts$': 'ts-jest', // Transform TypeScript files
     },
--- test/room.test.ts
+++ test/room.test.ts
@@ -1,24 +1,29 @@
 import { ColyseusTestServer, boot } from "@colyseus/testing";
 // import your "app.config.ts" file here.
 import appConfig from "../src/app.config";
-import { createNewPlayer} from "../src/players/db/Player";
-import { Client } from 'colyseus';
-import { ArraySchema, MapSchema } from "@colyseus/schema";
-import { AffectedStats } from "../src/common/schema/AffectedStatsSchema";
+import {getNextPlayerId } from "../src/players/db/Player";
+import mongoose from 'mongoose';
 
-jest.mock('../src/players/db/Player', () => ({
+/*jest.mock('../src/players/db/Player', () => ({
     getPlayer: jest.fn().mockResolvedValue(null),
     createNewPlayer: jest.fn()
-}));
+}));*/
 
 describe("testing your Colyseus app", () => {
     let colyseus: ColyseusTestServer;
 
-    beforeAll(async () => colyseus = await boot(appConfig));
+    beforeAll(async () => {
+        await mongoose.connect(process.env.DB_CONNECTION_STRING!, {
+            autoIndex: true,
+        });
+
+        colyseus = await boot(appConfig)
+    });
 
     afterAll(async () => {
         await colyseus.shutdown()
         jest.clearAllMocks();
+        mongoose.disconnect();
     });
 
     beforeEach(async () => {
@@ -28,63 +33,20 @@ describe("testing your Colyseus app", () => {
     it("connecting into a room", async () => {
 
         const mockOptions = {
-            playerId: 123, // Example player ID
+            playerId: await getNextPlayerId(), // Example player ID
             name: "Mocked Player", // Example player name
             avatarUrl: "mocked_avatar_url", // Optional avatar URL
         };
 
-        const talentsArray = new ArraySchema();
-
-        (createNewPlayer as jest.Mock).mockResolvedValue({
-            playerId: 123,
-            originalPlayerId: 123,
-            name: "Mocked Player",
-            sessionId: "mocked_session_id",
-            talents: talentsArray,  // Use real ArraySchema here
-            inventory: new ArraySchema(), // Real ArraySchema for inventory
-            lockedShop: new ArraySchema(),
-            equippedItems: new MapSchema(), // Real MapSchema for equippedItems
-            dodgeRate: 0,
-            refreshShopCost: 2,
-            maxHp: 100,
-            _hp: 100,
-            baseStats: new AffectedStats ({
-                strength: 3,
-                accuracy: 1,
-                maxHp: 100,
-                defense: 0,
-                attackSpeed: 0.8,
-                flatDmgReduction: 0,
-                dodgeRate: 0,
-                income: 0,
-                hpRegen: 0,
-            }),
-            damage: 0,
-            attackTimer: {},
-            poisonTimer: {},
-            regenTimer: {},
-            invincibleTimer: {},
-            talentsOnCooldown: [],
-            invincible: false,
-            rewardRound: 1,
-        });
-
         // Use `createRoom` to simulate creating a room instance on the server
         const room = await colyseus.createRoom("draft_room", {}); // createRoom("draft_room", {}) creates a new DraftRoom
 
-        room.state.remainingTalentPoints = 1;
         // Simulate client joining the room with `onJoin` method
-        const mockClient = { sessionId: "mocked_session_id" } as Client;  // Mock client with a sessionId
-
-        if (room.onJoin) {
-            await room.onJoin(mockClient, mockOptions); // Call onJoin with the mocked client and options
-        } else {
-            throw new Error("onJoin method is not defined in the room");
-        }
-
+        const client1 = await colyseus.connectTo(room, mockOptions);
+        expect(client1.sessionId).toEqual(room.clients[0].sessionId);
         // You can check that the room state is set up correctly after onJoin is triggered
-        expect(createNewPlayer).toHaveBeenCalledWith(mockOptions.playerId, mockOptions.name, mockClient.sessionId, mockOptions.avatarUrl);
-        expect(room.state.player).toBeDefined();
-        expect(room.state.remainingTalentPoints).toEqual(1); // Assuming your room initializes talent points
+        //expect(createNewPlayer).toHaveBeenCalledWith(mockOptions.playerId, mockOptions.name, mockClient.sessionId, mockOptions.avatarUrl);
+        //expect(room.state.player).toBeDefined();
+        //expect(room.state.remainingTalentPoints).toEqual(1); // Assuming your room initializes talent points
     });
 });
