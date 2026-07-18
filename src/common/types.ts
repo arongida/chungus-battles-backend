e07e93bebe1d30a6e86fff213d83b1726a38e500
arongida <arkheonos@gmail.com>
2026-07-18 20:42:20 +0200

    bump actual season var

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,4 +1,4 @@
-export const GAME_VERSION = 18;
+export const GAME_VERSION = 19;
 
 /** Reaching this many wins ends the run as a victory (Season 16 fixed-length runs). */
 export const WINS_TO_WIN = 12;
